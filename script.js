bebf95d47b0b9939c17029a24af153ecbcf60806
Olga <o.smolyakova30@gmail.com>
2022-12-04 13:57:23 -0600

    added animation

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 gsap.to("h1", {x:450, duration:2})
-gsap.from(".welcomeImg", {opasity:0, duration:3, stagger:1})
+gsap.from(".welcomeImg", {rotation:30, x:400, duration:1})
+gsap.from(".servicesImg", {opacity:0.1, duration:2, stagger:1, repeat:-1})
